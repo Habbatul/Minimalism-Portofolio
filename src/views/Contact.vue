f04da13118019f255dc37005461de40aa4e22736
Habbatul <gamernolifeneet@gmail.com>
2024-08-07 22:00:45 +0700

    feat : Change Everything to new Portofolio

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,7 +4,7 @@
         <p class="font-serif text-[1.65rem] mb-1">
         Contact Me
         </p>
-        <p class="font-serif">You can see my content and contact me through several social media below.</p>
+        <p class="font-serif text-[0.95rem] leading-[1.35rem]">You can see my content and contact me through several social media below.</p>
         <ul class="list-disc text-[1rem] md:text-[1.08rem] pl-4 my-4">
             <li><a href="https://www.linkedin.com/in/habbatul/" target="_blank" class="font-semibold hover:text-red-600">Linkedin : <span class="underline underline-offset-2">Habbatul Qolbi H</span></a></li>
             <li><a href="https://github.com/Habbatul" target="_blank" class="font-semibold hover:text-red-600">Github : <span class="underline underline-offset-2">Habbatul</span></a></li>
