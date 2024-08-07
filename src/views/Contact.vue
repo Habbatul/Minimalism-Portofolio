db9f330dfff7ce2f8b393c4bbc283004f047e83e
Habbatul <gamernolifeneet@gmail.com>
2024-08-08 02:40:21 +0700

    feat : Change Everything to new Portofolio

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="container max-w-lg mx-auto p-3 mt-4 text-lg">
         <div class="w-fit mx-auto px-2">
-        <p class="font-serif text-[1.65rem] mb-1">
+        <p class="font-serif text-[1.65rem] mb-2">
         Contact Me
         </p>
-        <p class="font-serif text-[0.95rem] leading-[1.35rem]">You can see my content and contact me through several social media below.</p>
+        <p class="font-serif text-[1.08rem] leading-[1.5rem]">You can see my content and contact me through several social media below.</p>
         <ul class="list-disc text-[1rem] md:text-[1.08rem] pl-4 my-4">
-            <li><a href="https://www.linkedin.com/in/habbatul/" target="_blank" class="font-semibold hover:text-red-600">Linkedin : <span class="underline underline-offset-2">Habbatul Qolbi H</span></a></li>
-            <li><a href="https://github.com/Habbatul" target="_blank" class="font-semibold hover:text-red-600">Github : <span class="underline underline-offset-2">Habbatul</span></a></li>
-            <li><a href="https://www.instagram.com/hq.han/" target="_blank" class="font-semibold hover:text-red-600">Instagram :<span class="underline underline-offset-2">@hq.han</span> </a></li>
-            <li><a href="https://www.youtube.com/@hq-han" target="_blank" class="font-semibold hover:text-red-600">Youtube : <span class="underline underline-offset-2">@hq.han</span></a></li>
+            <li><a href="https://www.linkedin.com/in/habbatul/" target="_blank" class="font-serif hover:text-red-600">Linkedin : <span class="underline underline-offset-2">Habbatul Qolbi H</span></a></li>
+            <li><a href="https://github.com/Habbatul" target="_blank" class="font-serif hover:text-red-600">Github : <span class="underline underline-offset-2">Habbatul</span></a></li>
+            <li><a href="https://www.instagram.com/hq.han/" target="_blank" class="font-serif hover:text-red-600">Instagram :<span class="underline underline-offset-2">@hq.han</span> </a></li>
+            <li><a href="https://www.youtube.com/@hq-han" target="_blank" class="font-serif hover:text-red-600">Youtube : <span class="underline underline-offset-2">@hq.han</span></a></li>
         </ul>
         </div>
 
